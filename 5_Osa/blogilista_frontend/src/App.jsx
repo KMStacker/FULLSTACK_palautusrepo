f35c0b7c2a800155f33d663afaf86082b43485c7
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-05-28 15:36:48 +0300

    5.5-5.6

--- 5_Osa/blogilista_frontend/src/App.jsx
+++ 5_Osa/blogilista_frontend/src/App.jsx
@@ -1,10 +1,13 @@
 // src/App.jsx
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import './index.css'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -12,10 +15,9 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
   const [notification, setNotification] = useState(null)
-  const [newBlogTitle, setNewBlogTitle] = useState('')
-  const [newBlogAuthor, setNewBlogAuthor] = useState('')
-  const [newBlogUrl, setNewBlogUrl] = useState('')
+  const [loginVisible, setLoginVisible] = useState(false)
 
+  const blogFormRef = useRef()
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -68,23 +70,19 @@ const App = () => {
       }, 5000)
   }
 
-  const handleCreateBlog = async (event) => {
-    event.preventDefault()
-    
+  const handleCreateBlog = async (blogObject) => {
+        
     try {
-      const newBlog = await blogService.create({
-        title: newBlogTitle,
-        author: newBlogAuthor,
-        url: newBlogUrl,
-      })
+      const newBlog = await blogService.create(blogObject)
       setBlogs(blogs.concat(newBlog))
-      setNewBlogTitle('')
-      setNewBlogAuthor('')
-      setNewBlogUrl('')
+ 
       setNotification({message: 'A new blog named as ' + newBlog.title + ' has been created!', type: 'notificationGood'})
       setTimeout(() => {
         setNotification(null)
       }, 5000)
+
+      blogFormRef.current.toggleVisibility()
+
     } catch (exception) {
       setNotification({message: 'Error occured while trying to create blog', type: 'notificationBad'})
       setTimeout(() => {
@@ -94,81 +92,43 @@ const App = () => {
     }
   }
 
-  const loginForm = () => (
-    <div>
-      <h2>log in to application</h2>
-    
-    <form onSubmit={handleLogin}>
-      <div>
-        username &nbsp;
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password &nbsp;
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-    </div>   
-  )
+  const loginForm = () => {
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showWhenVisible = { display: loginVisible ? '' : 'none' }
 
-  const blogForm = () => (
-    <div>
-      <h2>create new blog</h2>
-      <form onSubmit={handleCreateBlog}>
-        <div>
-          title: &nbsp;
-          <input
-            type="text"
-            value={newBlogTitle}
-            name="Title"
-            onChange={({ target }) => setNewBlogTitle(target.value)}
-          />
+    return (
+      <div>
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>log in</button>
         </div>
-        <div>
-          author: &nbsp;
-          <input
-            type="text"
-            value={newBlogAuthor}
-            name="Author"
-            onChange={({ target }) => setNewBlogAuthor(target.value)}
+        <div style={showWhenVisible}>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
           />
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
         </div>
-        <div>
-          url: &nbsp;
-          <input
-            type="text"
-            value={newBlogUrl}
-            name="Url"
-            onChange={({ target }) => setNewBlogUrl(target.value)}
-          />
-        </div>
-        <button type="submit">create</button>
-      </form>
-    </div>
-  )
+      </div>
+    )
+  }
 
   return (
     <div>
       <Notification notification={notification} />
       {!user && loginForm()}
       {user && <div>
-        <h2>Blogs</h2>
+        <h2 className= "BIG-blogs">Blogs</h2>
         <p>{user.name} logged in &nbsp;
         <button onClick={handleLogout}>logout</button>
         </p>
-        {blogForm()}
+        <Togglable buttonLabel="create a new blog" ref={blogFormRef}>
+        <BlogForm createBlog={handleCreateBlog} />
+        </Togglable>
         <br />
+        <h4 className="list-of-all-blogs">list of all blogs</h4>
         {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
         )}
--- 5_Osa/blogilista_frontend/src/components/BlogForm.jsx
+++ 5_Osa/blogilista_frontend/src/components/BlogForm.jsx
@@ -1,3 +1,58 @@
 // src/components/BlogForm.jsx
 import { useState } from 'react'
 
+const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+    await createBlog({ 
+      title: title,
+      author: author,
+      url: url 
+    })
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+  
+  return (
+    <div className="small-space-after">
+      <h2 className="create-a-new-blog">create a new blog</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title: &nbsp;
+          <input
+            type="text"
+            value={title}
+            name="Title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author: &nbsp;
+          <input
+            type="text"
+            value={author}
+            name="Author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div className="small-space-after">
+          url: &nbsp;
+          <input
+            type="text"
+            value={url}
+            name="Url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogForm
