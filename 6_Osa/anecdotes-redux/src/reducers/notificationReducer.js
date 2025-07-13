1979ed88b89d603353578efa4a19024c1d2cb822
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-07-13 14:40:15 +0300

    6.12

---
+++ 6_Osa/anecdotes-redux/src/reducers/notificationReducer.js
@@ -0,0 +1,12 @@
+// src/reducers/notificationReducer.js
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationReducer = createSlice({
+  name: 'notification',
+  initialState: 'halojata halloo',
+  reducers: {
+    //...
+  }
+})
+
+export default notificationReducer.reducer
