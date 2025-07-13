f883d34af74a255eb7c4a0c829b9adba5a43aaad
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-07-13 12:18:01 +0300

    6.9

--- 6_Osa/anecdotes-redux/src/App.jsx
+++ 6_Osa/anecdotes-redux/src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.jsx
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import Filter from './components/Filter'
 
 const App = () => {
 
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
