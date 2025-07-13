2c67428f84acab93ed4171ae468deaaa30d2fda2
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-07-13 13:21:19 +0300

    6.10

--- 6_Osa/anecdotes-redux/src/reducers/filterReducer.js
+++ 6_Osa/anecdotes-redux/src/reducers/filterReducer.js
@@ -1,17 +1,16 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+// src/reducers/filterReducer.js
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterReducer = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    filterChange(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
+export const { filterChange } = filterReducer.actions
 
-export default filterReducer
+export default filterReducer.reducer
