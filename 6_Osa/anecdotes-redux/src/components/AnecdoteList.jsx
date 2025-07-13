d4cb163d3a3cb277c035261b12b49a6dfbc9ba2f
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-07-13 14:58:22 +0300

    6.13

--- 6_Osa/anecdotes-redux/src/components/AnecdoteList.jsx
+++ 6_Osa/anecdotes-redux/src/components/AnecdoteList.jsx
@@ -1,8 +1,10 @@
+// src/components/AnecdoteList.jsx
 import { useSelector, useDispatch } from "react-redux"
 import { voteAnecdote } from "../reducers/anecdoteReducer"
+import {setNotification} from "../reducers/notificationReducer"
 
 const AnecdoteList = () => {
-  
+
   const anecdotes = useSelector(state => {
     if (state.filter === '') {
       return state.anecdotes
@@ -14,8 +16,9 @@ const AnecdoteList = () => {
 
   const dispatch = useDispatch()
 
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
+  const vote = (anecdote) => {
+    dispatch(voteAnecdote(anecdote.id))
+    dispatch(setNotification(`You just clicked vote on: '${anecdote.content}'`, 5))
   }
 
   return (
@@ -27,7 +30,7 @@ const AnecdoteList = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => vote(anecdote)}>vote</button>
           </div>
         </div>
       )}
--- 6_Osa/anecdotes-redux/src/reducers/notificationReducer.js
+++ 6_Osa/anecdotes-redux/src/reducers/notificationReducer.js
@@ -5,8 +5,24 @@ const notificationReducer = createSlice({
   name: 'notification',
   initialState: 'halojata halloo',
   reducers: {
-    //...
+    showNotification(state, action) {
+      return action.payload
+    },
+    hideNotification() {
+      return null
+    }
   }
 })
+
+export const { showNotification, hideNotification } = notificationReducer.actions
+
+export const setNotification = (text, time) => {
+  return async dispatch => {
+    dispatch(showNotification(text))
+    setTimeout(() => {
+      dispatch(hideNotification())
+    }, time * 1000)
+  }
+}
 
 export default notificationReducer.reducer
