2095ba4731ae9412cd35793de8b6833e2b245eb2
Fapacus <142172371+Fapacus@users.noreply.github.com>
2025-07-14 14:17:18 +0300

    6.14, 6.15

--- 6_Osa/anecdotes-redux/src/components/AnecdoteForm.jsx
+++ 6_Osa/anecdotes-redux/src/components/AnecdoteForm.jsx
@@ -2,22 +2,26 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const newAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    
+    const newAnecdote = await anecdoteService.createNew(content)
+
+    dispatch(createAnecdote(newAnecdote))
     dispatch(setNotification(`You just created: '${content}'`, 5))
   }
 
   return (
     <div>
       <h2>create new</h2>
-      <form onSubmit={newAnecdote}>
+      <form onSubmit={addAnecdote}>
         <div><input name="anecdote" /></div>
         <button type="submit">create</button>
       </form>
